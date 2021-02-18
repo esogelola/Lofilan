5e4e4d9a8d38b19ce8bba4ec86deea95ee8b988e
esogelola <esogelola@gmail.com>
2021-02-18 17:52:32 -0500

    Assets & Theme

---
+++ app/theme/mainTheme.js
@@ -0,0 +1,12 @@
+import {DefaultTheme} from 'react-native-paper';
+
+const mainTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    accent: 'yellow',
+  },
+};
+
+export default mainTheme;
