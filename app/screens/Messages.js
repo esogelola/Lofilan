97423cb1a7a8a6f902f0e83a40b9c32f5f7ce66b
yevheniizalizniak <72947197+yevheniizalizniak@users.noreply.github.com>
2021-04-05 22:36:44 -0400

    Functionality of messages added

--- app/screens/Messages.js
+++ app/screens/Messages.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import {SafeAreaView, View, Text, Button} from 'react-native';
 import firebase from '../firebase';
 export default function Messages({navigation}) {
@@ -9,4 +9,80 @@ export default function Messages({navigation}) {
       </View>
     </SafeAreaView>
   );
-}
+}*/
+import React from 'react';
+import { View, Text, Button, StyleSheet, FlatList } from 'react-native';
+import {
+  Container,
+  Card,
+  UserInfo,
+  UserImgWrapper,
+  UserImg,
+  UserInfoText,
+  UserName,
+  PostTime,
+  MessageText,
+  TextSection,
+} from '../styles/MessageStyles';
+import Header from '../components/elements/MainHeader';
+import { ScrollView } from 'react-native';
+const Messages = [
+
+  {
+    id: '2',
+    userName: 'Alisher Urazbayev',
+    userImg: require('../assets/users/user-2.jpg'),
+    messageTime: '2 hours ago',
+    messageText:
+      'Doing goood',
+  },
+  {
+    id: '3',
+    userName: 'Emmanuel Sogelola',
+    userImg: require('../assets/users/user-3.jpg'),
+    messageTime: '3 hours ago',
+    messageText:
+      'Nice to hear that, dog',
+  },
+ 
+];
+
+const MessagesScreen = ({navigation}) => {
+    return (
+
+      <Container>
+        <FlatList 
+       
+          data={Messages}
+          keyExtractor={item=>item.id}
+          renderItem={({item}) => (
+            <Card onPress={() => navigation.navigate('Chat', {userName: item.userName})}>
+              <UserInfo>
+                <UserImgWrapper>
+                  <UserImg source={item.userImg} />
+                </UserImgWrapper>
+                <TextSection>
+                  <UserInfoText>
+                    <UserName>{item.userName}</UserName>
+                    <PostTime>{item.messageTime}</PostTime>
+                  </UserInfoText>
+                  <MessageText>{item.messageText}</MessageText>
+                </TextSection>
+              </UserInfo>
+            </Card>
+          )}
+        />
+      </Container>
+      
+    );
+};
+
+export default MessagesScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    alignItems: 'center', 
+    justifyContent: 'center'
+  },
+});
