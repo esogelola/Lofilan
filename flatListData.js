2cb65c7c1c028412cf9a8016a2acc76bf141d392
AlisherUrazbayev <71267989+AlisherUrazbayev@users.noreply.github.com>
2021-04-07 00:16:54 -0400

    marketplace added

---
+++ flatListData.js
@@ -0,0 +1,19 @@
+var flatListData = [
+    {
+        "key": "1",
+        "name": "Sofa",
+        "descreption": "Good sofa for sale"
+    },
+    {
+        "key": "2",
+        "name": "Laptop",
+        "descreption": "Good laptop for sale"
+    },
+    {
+        "key": "3",
+        "name": "Iphone",
+        "descreption": "Good Iphone for sale"
+    }
+]
+
+module.exports = flatListData;
