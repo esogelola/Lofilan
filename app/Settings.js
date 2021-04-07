6259c98ee181020d12c2afd0a9cee91b7545b015
yevheniizalizniak <72947197+yevheniizalizniak@users.noreply.github.com>
2021-04-06 23:54:28 -0400

    Portfolio functionality added

---
+++ app/Settings.js
@@ -0,0 +1,212 @@
+import React, {useState, useEffect, useContext} from 'react';
+import {View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  SafeAreaView,} from 'react-native';
+import firebase from '../firebase';
+import UserContext from '../contexts/UserContext';
+
+const Settings = ({navigation, route}) => {
+
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [deleted, setDeleted] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const {user} = useContext(UserContext);
+  const fetchPosts = async () => {
+    try {
+      const list = [];
+
+      await firestore()
+        .collection('posts')
+        .where('userId', '==', route.params ? route.params.userId : user.uid)
+        .orderBy('postTime', 'desc')
+        .get()
+        .then((querySnapshot) => {
+        
+
+          querySnapshot.forEach((doc) => {
+            const {
+              userId,
+              post,
+              postImg,
+              postTime,
+              likes,
+              comments,
+            } = doc.data();
+            list.push({
+              id: doc.id,
+              userId,
+              userName: 'Test Name',
+              userImg,
+              postTime: postTime,
+              post,
+              postImg,
+              liked: false,
+              likes,
+              comments,
+            });
+          });
+        });
+
+      setPosts(list);
+
+      if (loading) {
+        setLoading(false);
+      }
+
+      console.log('Posts: ', posts);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+
+
+
+
+
+
+
+  async function logoutUser() {
+    try {
+      await firebase.logout();
+
+      console.log('You have logged out successfully.');
+    } catch (err) {
+      console.error('Logout Error', err);
+    }
+  }
+  return (
+  <SafeAreaView style={{flex: 1}}>
+      <ScrollView
+        style={styles.container}
+        contentContainerStyle={{justifyContent: 'center', alignItems: 'center'}}
+        showsVerticalScrollIndicator={false}>
+        <Image
+          style={styles.userImg}
+          source={require('../assets/users/user-1.jpg')}
+        />
+        <Text style={styles.userName}>{userData ? userData.fname || 'Test' : 'Yevhenii'} {userData ? userData.lname || 'Name' : 'Zalizniak'}</Text>
+        {/* <Text>{route.params ? route.params.userId : user.uid}</Text> */}
+        <Text style={styles.aboutUser}>
+        {userData ? userData.about || 'No details added.' : 'Developing myself and this app)'}
+        </Text>
+        <View style={styles.userBtnWrapper}>
+          {route.params ? (
+            <>
+              <TouchableOpacity style={styles.userBtn} onPress={() => {}}>
+                <Text style={styles.userBtnTxt}>Message</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.userBtn} onPress={() => {}}>
+                <Text style={styles.userBtnTxt}>Follow</Text>
+              </TouchableOpacity>
+            </>
+          ) : (
+            <>
+            <View style={styles.userBtnWrapper}>
+              <TouchableOpacity
+                style={[styles.userBtn, {backgroundColor:'#989EB1'}]}
+                onPress={() => {
+                  
+                }}>
+                <Text style={[styles.userBtnTxt, {backgroundColor:'#989EB1'}]}>Edit</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+               style={styles.userBtn} onPress={() => logoutUser()}>
+                <Text style={styles.userBtnTxt}>Logout</Text>
+              </TouchableOpacity>
+              </View>
+            </>
+          )}
+        </View>
+
+        <View style={styles.userInfoWrapper}>
+         
+          <View style={styles.userInfoItem}>
+            <Text style={styles.userInfoTitle}>0</Text>
+            <Text style={styles.userInfoSubTitle}>Communities</Text>
+          </View>
+          <View style={styles.userInfoItem}>
+            <Text style={styles.userInfoTitle}>2</Text>
+            <Text style={styles.userInfoSubTitle}>Followers</Text>
+          </View>
+          
+        </View>
+
+        {posts.map((item) => (
+          <PostCard key={item.id} item={item} onDelete={handleDelete} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default  Settings;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f6f7',
+    padding: 20,
+  },
+  userImg: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+  },
+  userName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  aboutUser: {
+    fontSize: 12,
+    fontWeight: '600',
+    color: '#666',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  userBtnWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
+    marginBottom: 10,
+  },
+  userBtn: {
+    backgroundColor: '#F85F6A',
+    borderRadius: 3,
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    marginHorizontal: 5,
+  },
+  userBtnTxt: {
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+  userInfoWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    marginVertical: 20,
+  },
+  userInfoItem: {
+    justifyContent: 'center',
+  },
+  userInfoTitle: {
+    fontSize: 28,
+    color: '#989EB1',
+    fontWeight: 'bold',
+    marginBottom: 5,
+    textAlign: 'center',
+  },
+  userInfoSubTitle: {
+    fontSize: 14,
+    color: '#000000',
+    textAlign: 'center',
+  },
+});
